79d967b3d7aa3a1d21f3dc3f3bfd9a613493e9e6
BeanWei <157318439@qq.com>
2018-10-15 12:06:09 +0800

    windows 定时任务

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-::windows下执行定时任务>>>schtasks /create /tn "win_crontab" /tr "run.bat" /sc daily /st 12:30
+::windows瀹氭椂浠诲姟鎸囦护>>>schtasks /create /tn "freeSSR" /tr "run.bat" /sc hourly /mo 3
 go run doub.go
