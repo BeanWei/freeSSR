a99790dbfc8854f28faa521cb15629e785dcef74
BeanWei <157318439@qq.com>
2018-10-16 09:20:30 +0800

    "ssr节点分享，每日更新"

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+﻿@echo off 
 ::windows定时任务指令>>>schtasks /create /tn "freeSSR" /tr "run.bat" /sc hourly /mo 3
 ::删除定时任务指令>>>schtasks /delete /tn "freeSSR"
-go run doub.go
+    go run doub.go
