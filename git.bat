c7a30055e6e7fb15855423def860e12fa6742d6e
BeanWei <157318439@qq.com>
2018-10-18 09:34:52 +0800

    git 提交

---
+++ git.bat
@@ -0,0 +1,15 @@
+cd C:\Users\Administrator\Desktop\Bean\Go\src\github.com\BeanWei\SSR
+
+go run doub.go
+
+timeout 2
+
+git add C:\Users\Administrator\Desktop\Bean\Go\src\github.com\BeanWei\SSR\README.md
+
+git commit -m "ssr节点分享，每日更新"
+
+git remote add origin git@github.com:BeanWei/freeSSR.git
+
+git pull origin master
+
+git push origin master
--- run.vbs
+++
@@ -1,1 +0,0 @@
-CreateObject("WScript.Shell").Run "cmd /c go run C:\Users\Administrator\Desktop\Bean\Go\src\github.com\BeanWei\SSR\doub.go",1
